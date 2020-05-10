d4c53ede85e8db4966a04d4a44f033b499011af7
Chiara <ada.chiara.capuano@gmail.com>
2020-05-10 15:59:08 -0700

    implemented flask

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from Suggestions import Suggestions
 import pandas as pd
 df = pd.read_csv(r'./Netflix_Movies_All_Tags.csv', encoding="utf-8")
 df_title = pd.read_csv(r'./Movie_Titles.csv', encoding="utf-8")
-suggestions = Suggestions(df, df_title, "cats")
-print(suggestions.display_results())
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -32,11 +30,28 @@ def predict():
 
 @app.route("/")
 def home():
-    suggestions = Suggestions(df, df_title, "cats")
+    return """
+    <html><head></head>
+    <body>
+        <h1>Fuck you and the horse</h1>
+        <h3>you rode in on</h3>
+        <div>
+            <form action="/api/suggest" method="get">
+                <label for="q">Query:</label><br>
+                <input type="text" id="q" name="q" value=""><br>
+            </form>
+        </div>
+        </body>
+    </html>
+           """
 
-    return suggestions.display_results()
-
+@app.route("/api/suggest")
+def Suggest():
+    q = request.args.get('q')
+    #suggestions = Suggestions(df, df_title, "cats")
+    return q#suggestions.display_results()
+    #    output=model.predict(data)  #what you want to do with frozen model goes here"""
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
